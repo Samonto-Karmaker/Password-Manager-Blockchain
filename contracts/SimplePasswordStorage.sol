cf59e6a0135fbc2c89d00971ac0b27d92455c671
Samonto-Karmaker <samonto0726@gmail.com>
2025-04-23 18:15:29 +0600

    Refactor: improve error handling for users with no password

--- contracts/SimplePasswordStorage.sol
+++ contracts/SimplePasswordStorage.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+error NotFound();
+
 contract SimplePasswordStorage {
-    mapping (address => bytes32) private keyToPassword;
+    mapping(address => bytes32) private keyToPassword;
 
     function setPassword(string memory _password) external {
         bytes32 hashedPassword = keccak256(abi.encodePacked(_password));
@@ -10,6 +12,8 @@ contract SimplePasswordStorage {
     }
 
     function getPasswordById(address _user) external view returns (bytes32) {
-        return keyToPassword[_user];
+        bytes32 storedPassword = keyToPassword[_user];
+        if (storedPassword == bytes32(0)) revert NotFound();
+        return storedPassword;
     }
 }
