2645f89cc5e5f163bee683366b3f89e0ee140968
Samonto-Karmaker <samonto0726@gmail.com>
2025-04-23 17:37:40 +0600

    feat: implement basic password getter and setter

--- contracts/SimplePasswordStorage.sol
+++ contracts/SimplePasswordStorage.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.8;
 
 contract SimplePasswordStorage {
-    
+    mapping (address => bytes32) private keyToPassword;
+
+    function setPassword(string memory _password) external {
+        bytes32 hashedPassword = keccak256(abi.encodePacked(_password));
+        keyToPassword[msg.sender] = hashedPassword;
+    }
+
+    function getPasswordById(address _user) external view returns (bytes32) {
+        return keyToPassword[_user];
+    }
 }
